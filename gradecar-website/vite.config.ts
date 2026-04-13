516ef4a55a4b21fcf8c02a082e3708a504ce653e
Manish Gayen <125684097+GayenManish07@users.noreply.github.com>
2026-04-13 20:17:31 +0530

    Update vite.config.ts

--- gradecar-website/vite.config.ts
+++ gradecar-website/vite.config.ts
@@ -1,8 +1,16 @@
-export default defineConfig(({mode}) => {
-  // ...
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig} from 'vite';
+
+export default defineConfig(() => {
   return {
-    base: '/GraDeCAR/', // Add this line!
+    base: '/GraDeCAR/',
     plugins: [react(), tailwindcss()],
-    // ...
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
+      },
+    }
   };
 });
